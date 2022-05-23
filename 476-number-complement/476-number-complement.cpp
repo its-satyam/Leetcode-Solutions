768738760c41dac4bd9da81d84f6e6dadbd5c4d7
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-05-23 23:39:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.16%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int findComplement(int num) {
-        vector<int> arr;
-        while(num>0) {
-            arr.push_back(num % 2);
-            num /= 2;
+        // vector<int> arr;
+        // while(num>0) {
+        //     arr.push_back(num % 2);
+        //     num /= 2;
+        // }
+        // int sz = arr.size();
+        // reverse(arr.begin(), arr.end());
+        // for(int i = 0; i<sz; ++i) {
+        //       arr[i] = !arr[i];
+        // }
+        // long long t = 1;
+        // for(int i = sz-1; i>=0; --i) {
+        //     num += arr[i] * t;
+        //     t *= 2;
+        // }
+        // return num;
+        
+        int i=0,j=0;
+        while(i<num)
+        {
+            i=i+ pow(2,j);
+            j++;
         }
-        int sz = arr.size();
-        reverse(arr.begin(), arr.end());
-        for(int i = 0; i<sz; ++i) {
-              arr[i] = !arr[i];
-        }
-        long long t = 1;
-        for(int i = sz-1; i>=0; --i) {
-            num += arr[i] * t;
-            t *= 2;
-        }
-        return num;
+    return i-num;
     }
 };
