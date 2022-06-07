feb7563ecf0a6cddaabd137997cd7d7c3ed6c94a
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-06-07 13:56:34 +0530

    Time: 26 ms (21.67%), Space: 14.4 MB (30.12%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -2,25 +2,33 @@ class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) 
     {
-        int count1=0;
-        for(int i=0;i<arr1.size();++i)
-        {
-            int count=0;
-            for(int j=0;j<arr2.size();++j)
-            {
-                if(abs(arr1[i]-arr2[j])<=d)
-                {
-                    count++;
-                    break;
-                }
-            }
+//         int count1=0;
+//         for(int i=0;i<arr1.size();++i)
+//         {
+//             int count=0;
+//             for(int j=0;j<arr2.size();++j)
+//             {
+//                 if(abs(arr1[i]-arr2[j])<=d)
+//                 {
+//                     count++;
+//                     break;
+//                 }
+//             }
+//             if(count==0){
+//                 count1++;
+//             }
             
-            if(count==0){
-                count1++;
-            }
-            
-        }
-        return count1;
+//         }
+//         return count1;
 
+        
+        set<int> s;
+        int n1 = arr1.size(), n2 = arr2.size(), ans = 0;
+        for(int i=0; i<n2; i++) 
+            s.insert(arr2[i]);
+        for(int i=0; i<n1; i++) 
+            if(s.lower_bound(arr1[i]-d) == s.upper_bound(arr1[i]+d))
+                ans++;
+        return ans;
     }
 };
