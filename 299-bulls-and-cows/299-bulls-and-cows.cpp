1c77b920fad3829e46c7f538129b0a8b65c8abab
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-07-02 21:56:46 +0530

    Time: 8 ms (43.10%), Space: 6.5 MB (94.33%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+          int n = secret.size(), a[10] = {0},b[10] = {0},bulls = 0,cows = 0;
+        for(int i = 0;i<n;i++)
+            if(secret[i] == guess[i]) bulls++;
+            else{
+                a[secret[i] - '0']++;
+                b[guess[i] - '0']++;
+            }
+        for(int i = 0;i<10;i++) cows += min(a[i],b[i]);
+        return to_string(bulls) + 'A' + to_string(cows) + 'B';
+    }
+};
