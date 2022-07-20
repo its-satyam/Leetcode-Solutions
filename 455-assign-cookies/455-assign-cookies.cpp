457e11bd30eea4323a275bafe2b7e3bf7999007f
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-07-20 21:10:31 +0530

    Time: 58 ms (18.21%), Space: 17.5 MB (31.48%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) 
+    {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0,j=0,c=0;
+        while(i<g.size() && j<s.size())
+        {
+            if(g[i]<=s[j])
+            {
+                c++;
+                i++;
+                j++;
+            }
+            else
+                j++;
+                
+        }
+        return c;
+    }
+};
