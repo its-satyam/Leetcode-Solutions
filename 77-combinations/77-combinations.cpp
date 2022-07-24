64ca18811ee874c4f5a1de1d862333a512b8737b
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-07-24 20:32:54 +0530

    Time: 52 ms (38.72%), Space: 8.8 MB (97.88%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+      vector<vector<int>> res;
+        vector<int> temp(k, 0);
+        int itr = 0;
+        while(itr > -1){
+            temp[itr]++;
+            if(temp[itr] > n) itr--;
+            else if(itr == k-1) res.push_back(temp);
+            else{                
+                itr++;
+                temp[itr] = temp[itr-1];
+            }
+        }
+        return res;   
+    }
+};
