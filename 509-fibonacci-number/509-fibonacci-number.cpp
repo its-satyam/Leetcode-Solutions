f99b8e379b7574afbc3ae56eb92613ee7157ea68
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-08-04 23:49:24 +0530

    Time: 3 ms (58.90%), Space: 5.9 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
-        else
-            return fib(n-1)+fib(n-2);
+        // if(n==0)
+        //     return 0;
+        // else if(n==1)
+        //     return 1;
+        // else
+        //     return fib(n-1)+fib(n-2);
+        
+        int dp[2] = {0, 1};
+        for (int i = 2; i <= n; ++i)
+        dp[i%2] += dp[(i+1)%2] ;
+        return dp[n%2];
     }
 };
