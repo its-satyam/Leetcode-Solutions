9dea97c9a60380c4a5cd4fb31833a4a5aed8f635
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-08-13 11:23:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (63.58%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     vector<int> distributeCandies(int candies, int num_people) {
-        vector<int> res(num_people, 0); // start with zero candies for each person
-        int i=0;
-        while (candies > 0) // while there are more candies to distribute
-        {
-            res[i%num_people]+=i+1;
-            candies-=i+1;
-            i++;
-        }
-        if (candies < 0) // if the last person got more than the candies we had left
-            res[(i-1)%num_people] -= abs(candies);
+         vector<int> res(num_people);
+        for ( int index = 0; candies > 0; ++index, candies -= index )
+            res[index % num_people] += min(index + 1, candies);
         return res;
     }
 };
