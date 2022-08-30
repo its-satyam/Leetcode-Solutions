3f48b0e3361dfffac6eaf64b76720d2e28132322
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-08-30 22:46:24 +0530

    Time: 13 ms (50.96%), Space: 9 MB (28.55%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int count=0;
-        for(int i=0;i<nums.size()-2;i++)
+        // int count=0;
+        // for(int i=0;i<nums.size()-2;i++)
+        // {
+        //     for(int j=i+1;j<nums.size()-1;j++)
+        //     {
+        //         for(int k=j+1;k<nums.size();k++)
+        //         {
+        //             if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
+        //                 count++;
+        //         } 
+        //     }
+        // }
+        // return count;
+        
+        int cnt = 0;
+        unordered_set<int> set;
+        for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size()-1;j++)
+            set.insert(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(set.find(nums[i]+diff)!=set.end() && set.find(nums[i]+diff+diff)!=set.end())
             {
-                for(int k=j+1;k<nums.size();k++)
-                {
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
-                        count++;
-                } 
+                cnt++;
             }
         }
-        return count;
+        return cnt;
     }
 };
