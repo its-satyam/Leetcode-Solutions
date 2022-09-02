e896fc74a22d40e5c776e26af20d0799f0727c42
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-09-02 22:58:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (57.34%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==' ')
+            {
+                k--;
+                if(k==0)
+                    break;
+            }
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
