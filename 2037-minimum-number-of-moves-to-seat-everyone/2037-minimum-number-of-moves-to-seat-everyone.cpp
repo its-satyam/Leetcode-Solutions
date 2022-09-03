8fcfd595c4e3bfcad400118153261304e5647051
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-09-03 17:43:17 +0530

    Time: 16 ms (56.14%), Space: 18 MB (86.97%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int ans=0;
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        for(int i=0;i<students.size();i++)
+        {
+            ans+=abs(students[i]-seats[i]);
+        }
+        return ans;
+    }
+};
