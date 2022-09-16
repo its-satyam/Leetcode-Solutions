32e1c248cd836693b683d2a8bd1cdf4a1b42d040
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-09-16 17:32:18 +0530

    Time: 15 ms (82.49%), Space: 10.8 MB (46.66%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i,j,n;
-        n=nums.size();
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                    return{i,j};
-            }
-        }
-        return{};
+        // int i,j,n;
+        // n=nums.size();
+        // for(int i=0;i<n-1;i++)
+        // {
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //             return{i,j};
+        //     }
+        // }
+        // return{};
         
-//         vector<int> ans;
-// 		unordered_map<int, int> map;
+        vector<int> ans;
+		unordered_map<int, int> map;
 
-// 		for(int i=0; i<nums.size(); i++){
-// 			if(map.find(target - nums[i]) != map.end()){
-// 				ans.push_back(map[target - nums[i]]);
-// 				ans.push_back(i);
-// 				return ans;
-// 			}
-// 			else 
-// 				map[nums[i]] = i;
-// 		}
+		for(int i=0; i<nums.size(); i++){
+			if(map.find(target - nums[i]) != map.end()){
+				ans.push_back(map[target - nums[i]]);
+				ans.push_back(i);
+				return ans;
+			}
+			else 
+				map[nums[i]] = i;
+		}
 
-// 		return ans;
+		return ans;
             
     }
 };
