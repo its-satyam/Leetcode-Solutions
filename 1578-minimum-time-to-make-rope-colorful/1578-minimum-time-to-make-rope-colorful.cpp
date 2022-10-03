59570c3db159993073686797e32b0829792974f9
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2022-10-03 22:51:49 +0530

    Time: 340 ms (30.71%), Space: 95.4 MB (84.72%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        vector<int> temp;
-        int sn=colors.length();
-        int sub=0,sum=0;
-        for(int i=0;i<sn-1;i++)
-        {
-            if(colors[i]==colors[i+1])
+        // vector<int> temp;
+        // int sn=colors.length();
+        // int sub=0,sum=0;
+        // for(int i=0;i<sn-1;i++)
+        // {
+        //     if(colors[i]==colors[i+1])
+        //     {
+        //         temp.push_back(neededTime[i]);
+        //         if(i+1==sn-1)
+        //         {
+        //             temp.push_back(neededTime[i+1]);
+        //             sub+=*max_element(temp.begin(),temp.end());
+        //             temp.clear();
+        //         }
+        //     }
+        //     else if(colors[i]!=colors[i+1])
+        //     {
+        //         temp.push_back(neededTime[i]);
+        //         sub+=*max_element(temp.begin(),temp.end());
+        //         temp.clear();
+        //         if(i+1==sn-1)
+        //             sub+=neededTime[i+1];
+        //     }
+        //     sum+=neededTime[i];
+        // }
+        // sum+=neededTime[sn-1];
+        // return sum-sub;
+        
+        int answer=0;
+         for(int i=1;i<neededTime.size();i++)
+         {
+            if(colors[i-1]==colors[i])
             {
-                temp.push_back(neededTime[i]);
-                if(i+1==sn-1)
-                {
-                    temp.push_back(neededTime[i+1]);
-                    sub+=*max_element(temp.begin(),temp.end());
-                    temp.clear();
-                }
+                answer+=min(neededTime[i],neededTime[i-1]);
+                neededTime[i]=max(neededTime[i],neededTime[i-1]);
             }
-            else if(colors[i]!=colors[i+1])
-            {
-                temp.push_back(neededTime[i]);
-                sub+=*max_element(temp.begin(),temp.end());
-                temp.clear();
-                if(i+1==sn-1)
-                    sub+=neededTime[i+1];
-            }
-            sum+=neededTime[i];
-        }
-        sum+=neededTime[sn-1];
-        return sum-sub;
+         } 
+         return answer;
     }
 };
