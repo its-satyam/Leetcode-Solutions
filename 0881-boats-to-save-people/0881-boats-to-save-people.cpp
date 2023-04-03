f34542e35b5370c3154239f38315c83e3a15d0f8
Satyam Gupta <84875070+its-satyam@users.noreply.github.com>
2023-04-03 23:41:05 +0530

    Time: 91 ms (48.71%), Space: 42 MB (59.84%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int res = 0;
+        int l = 0, r = people.size()-1;
+        while (l <= r) {
+            if (people[l] + people[r] <= limit)
+                l++;
+            res++;
+            r--;
+        }
+        return res;
+    }
+};
